c825fba86d2403807e6cac039d2119447b71c528
Kenny <ckennyc@tamu.edu>
2020-06-02 18:58:33 -0500

    test importing module

--- nodes/path_planner_node.py
+++ nodes/path_planner_node.py
@@ -2,3 +2,12 @@
 
 import rospy
 
+
+from tamu_sa.search.search_algorithms import AStarSearch
+from tamu_sa.graphs.graph import SquareGrid
+from tamu_sa.graphs.ogm import OccupancyGridMap
+
+
+rospy.init_node("planner_test_node")
+
+rospy.spin()
